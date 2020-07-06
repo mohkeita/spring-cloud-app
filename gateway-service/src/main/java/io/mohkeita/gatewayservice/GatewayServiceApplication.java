46c34f8adcf546e0b164d3d65f3962a90e1f2e19
mohkeita <mohkeita13@gmail.com>
2020-07-06 16:55:29 +0200

    configure the dynamic routes

--- gateway-service/src/main/java/io/mohkeita/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/io/mohkeita/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package io.mohkeita.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +16,18 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
-	@Bean
+	//@Bean
 	RouteLocator staticRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(r->r.path("/customers/**").uri("http://localhost:8081/").id("r1"))
-				.route(r->r.path("/products/**").uri("http://localhost:8082/").id("r2"))
+				.route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE").id("r1"))
+				.route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE").id("r2"))
 				.build();
 
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
 	}
 
 }
